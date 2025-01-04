0bf3237a2744d391103171765e088da780225a9b
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2025-01-04 16:31:48 +0600

    tailwind config update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "blue-primary": "#3C55A5",
+        "green-primary": "#1BB68A",
+        "green-light": "#E8F8F3",
+        "gray-light": "#4B5563",
+      },
+      container: {
+        screens: {
+          DEFAULT: "1290px",
+        },
+        center: true,
+        padding: "1.2rem",
+      },
+      screens: {
+        xs: "540px", 
       },
     },
   },
+  
   plugins: [],
 } satisfies Config;
