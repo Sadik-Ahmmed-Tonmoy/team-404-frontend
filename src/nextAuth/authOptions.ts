71efc42ca218fcd2b96a6aaf80a16f323423a27c
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2025-01-06 11:09:38 +0600

    problem solved

--- src/nextAuth/authOptions.ts
+++ src/nextAuth/authOptions.ts
@@ -1,5 +1,8 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import AppleProvider from "next-auth/providers/apple";
+
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -7,6 +10,14 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_Google_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET as string,
+    }),
+    AppleProvider({
+      clientId: process.env.NEXT_PUBLIC_APPLE_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_APPLE_SECRET as string,
     }),
   ],
   
